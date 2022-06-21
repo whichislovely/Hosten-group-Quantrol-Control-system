a2b79988c86eb01ef88d33dfc667fd831fbe544e
vli <vli@ist.ac.at>
2022-06-21 11:56:39 +0200

    testing

--- source_code.py
+++ source_code.py
@@ -150,6 +150,7 @@ class MainWindow(QMainWindow):
 
     def some_dummy_function():
         print("You called a dummy function")
+        print("this one was added from PC")
 
     #SEQUENCE TAB RELATED
     def sequence_table_changed(self, item):
