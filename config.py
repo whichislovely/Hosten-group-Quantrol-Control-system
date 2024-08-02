35a957fd964969d4d5c7c5d76a4b5064fb8ebe8e
whichislovely <107917886+whichislovely@users.noreply.github.com>
2024-08-02 11:39:32 +0200

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-digital_channels_number = 16
-analog_channels_number = 16
-dds_channels_number = 12
-artiq_environment_name = "artiq" # cold atoms team has "artiq_5". Hybrid experiment has "artiq"
-analog_card = "fastino" # cold atoms team has "zotino". Hybrid experiment has "fastino"
-research_group_name = "Hosten"
-allow_skipping_images = False
