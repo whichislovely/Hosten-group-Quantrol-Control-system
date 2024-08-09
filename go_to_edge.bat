55640463b011aeb0126c7a5285382e61b8982f77
hostenlab <hostenlab@wdonho031.ista.ac.at>
2024-08-09 16:07:39 +0200

    added an option for Clang users as conda is going to be deprecated

---
+++ go_to_edge.bat
@@ -0,0 +1,3 @@
+@echo OFF
+set "MSYSTEM=CLANG64"
+"C:\msys64\msys2_shell.cmd" -c "artiq_run go_to_edge.py"
---
+++ run_experiment.bat
@@ -0,0 +1,3 @@
+@echo OFF
+set "MSYSTEM=CLANG64"
+"C:\msys64\msys2_shell.cmd" -c "artiq_run run_experiment.py"
