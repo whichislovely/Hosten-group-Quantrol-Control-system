4c65e8601bb644f527beab95f12d907d84718cc4
vyacheslav <vli@ista.ac.at>
2024-09-05 15:18:55 +0200

    added the hybrid experiment config file

--- experiment specific files/hybrid experiment/config.py
+++ experiment specific files/hybrid experiment/config.py
@@ -22,11 +22,36 @@ slow_dds_channels = [
 ] # The sequence of the channels should be corresponding to the sequence in the slow DDS tab. The first one in the slow_dds_channels list will be the slow_DDS0 and so on
 sampler_channels_number = 8
 package_manager = "conda" #it can be either conda or clang64
-artiq_environment_name = "artiq_5" # it can be either artiq or artiq_5 for Hosten lab systems
+artiq_environment_name = "artiq" # it can be either artiq or artiq_5 for Hosten lab systems
 analog_card = "fastino" # it can be either fastino or zotino for Hosten lab systems
 research_group_name = "Hosten"
 allow_skipping_images = True
 list_of_devices_for_initialization = [
+    "urukul0_cpld",
+    "urukul0_ch0",
+    "urukul0_ch1",
+    "urukul0_ch2",
+    "urukul0_ch3",
+    "urukul1_cpld",
+    "urukul1_ch0",
+    "urukul1_ch1",
+    "urukul1_ch2",
+    "urukul1_ch3",
+    "urukul2_cpld",
+    "urukul2_ch0",
+    "urukul2_ch1",
+    "urukul2_ch2",
+    "urukul2_ch3",
+    "mirny0_cpld",
+    "mirny0_ch0",
+    "mirny0_ch1",
+    "mirny0_ch2",
+    "mirny0_ch3",
+    "fastino0",
+    "sampler0"
+]
+
+list_of_devices_for_use = [
     "core",
     "urukul0_cpld",
     "urukul0_ch0",
@@ -48,6 +73,11 @@ list_of_devices_for_initialization = [
     "urukul3_ch1",
     "urukul3_ch2",
     "urukul3_ch3",
+    "mirny0_cpld",
+    "mirny0_ch0",
+    "mirny0_ch1",
+    "mirny0_ch2",
+    "mirny0_ch3",
     "ttl0",
     "ttl1",
     "ttl2",
